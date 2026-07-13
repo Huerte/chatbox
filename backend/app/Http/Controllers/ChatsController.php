4195a9261534e0fa544384630a9edd418e50f0eb
Huerte <huertejerald@gmail.com>
2026-07-13 15:49:43 +0800

    chore: add remaining chats model templates

---
+++ backend/app/Http/Controllers/ChatsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chats;
+use Illuminate\Http\Request;
+
+class ChatsController extends Controller
+{
+    
+    public function index() {
+        $chats = Chats::with('user')->orderBy('created_at', 'asc')
+        ->get();
+
+        return view('chat.index', ['chats' => $chats]);
+    }
+
+    public function store(Request $request) {
+        
+        $validated = $request->validate([
+            'message' => ['required', 'max:500'],
+        ]);
+
+        Chats::create([
+            'user_id' => auth()->id,
+            'message' => $validated['message']
+        ]);
+
+        return redirect()->route('chat.index');
+
+    }
+
+    public function destroy(Chats $chat) {
+        $this->authorize('delete', $chat);
+        
+        $chat->delete();
+
+        return redirect()->route('chat.index');
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChatsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/chat', [ChatsController::class, 'index'])->name('chat.index');
+    Route::post('/chat', [ChatsController::class, 'store'])->name('chat.store');
+    Route::delete('/chat/{chat}', [ChatsController::class, 'destroy'])->name('chat.destroy');
 });
 
 require __DIR__.'/auth.php';
