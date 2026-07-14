5d1c48abc1667d914d0224acf0b714c7ae724950
Huerte <huertejerald@gmail.com>
2026-07-14 13:52:10 +0800

    chore: add docker for deployment

---
+++ backend/scripts/00-laravel-deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html --optimize-autoloader
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Linking storage..."
+php artisan storage:link
+
+echo "Running migrations..."
+php artisan migrate --force
