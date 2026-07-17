00d74918852a124f9d5b0eb42f9759c437e5a71a
Huerte <huertejerald@gmail.com>
2026-07-17 17:35:36 +0800

    chore: ensure storage permissions in docker entrypoint

--- backend/scripts/entrypoint.d/10-laravel.sh
+++ backend/scripts/entrypoint.d/10-laravel.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+chown -R application:application /app/storage /app/bootstrap/cache
+chmod -R 775 /app/storage /app/bootstrap/cache
+
 php artisan config:cache
 php artisan route:cache
 php artisan storage:link
